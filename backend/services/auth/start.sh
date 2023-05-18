2c449cb34d675668bffc71a68c7729b78c98dbb8
bgdnvk <bnovykov@gmail.com>
2023-05-18 05:19:24 +0200

    app and db cleanup

--- backend/services/auth/start.sh
+++ backend/services/auth/start.sh
@@ -2,5 +2,5 @@
 kubectl create secret generic secretenv --from-env-file=secret.env
 # start deployments
 kubectl apply -f loadbalancer.yaml
-kubectl apply -f postgresql.yaml
-kubectl apply -f deployment.yaml
+kubectl apply -f ./db/postgresql.yaml
+kubectl apply -f ./app/deployment.yaml
