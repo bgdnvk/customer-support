00fe9bf45c367c2b5a73bae3d7c5448cc6c48bab
bgdnvk <bnovykov@gmail.com>
2023-05-20 18:23:41 +0200

    k8s local bug, need to delte all --all

--- backend/services/auth/stop.sh
+++ backend/services/auth/stop.sh
@@ -3,3 +3,7 @@ kubectl delete deployments --all
 # encoding issues as well
 kubectl delete secret secretenv
 kubectl delete services --all
+
+# for some reason pods keep on running ????
+# delete everything (several times) with:
+kubectl delete all --all
