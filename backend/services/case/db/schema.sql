259607b2942dd83a637c082212c02a8791f5a1a6
bgdnvk <bnovykov@gmail.com>
2023-05-21 02:17:37 +0200

    db and dockerfile

---
+++ backend/services/case/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cases (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  resolved BOOLEAN DEFAULT FALSE,
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
